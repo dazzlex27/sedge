e20a4f8aa78f8a2aec63ca4c985cf9e7206bb12e
dazzlex27 <dazzlex27@mail.ru>
2017-08-10 00:28:04 +0500

    Фикс для скриптов.

--- Tools/list_all_by_project.py
+++ Tools/list_all_by_project.py
@@ -9,11 +9,11 @@ print("File list by project:")
 print("===============")
 for directory in dir_list:
     for (dir,_, files) in os.walk("..\\" + directory):
-                for f in files:
-                        path = os.path.join(dir, f)
-                        if os.path.exists(path) and path.endswith(ext_list):
-                                print(path)
-                                file_count += 1
+        for f in files:
+            path = os.path.join(dir, f)
+            if os.path.exists(path) and path.endswith(ext_list):
+                print(path)
+                file_count += 1
 print("===============")
 print("Total file count = ", file_count)
 input("")
--- Tools/total_line_count.py
+++ Tools/total_line_count.py
@@ -10,13 +10,12 @@ print("===============")
 for directory in dir_list:
     count = 0
     for (dir,_, files) in os.walk("..\\" + directory):
-                for f in files:
-                        path = os.path.join(dir, f)
-                        if os.path.exists(path) and path.endswith(ext_list):
-                                count += len(open(path).readlines(  ))
-    totalCount +=count
+            for f in files:
+                path = os.path.join(dir, f)
+                if os.path.exists(path) and path.endswith(ext_list):
+                    count += len(open(path).readlines(  ))
+    total_count += count
     print(directory + ": ", count)
 print("===============")
 print("Total count = ", total_count)
 input("")
-
