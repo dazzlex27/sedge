92c616c2498862076c32d892554a95bd91b5f974
dazzlex27 <dazzlex27@mail.ru>
2017-08-09 23:43:54 +0500

    Добавлены примитивные скрипты для метрик кода.

---
+++ Tools/list_all_by_project.py
@@ -0,0 +1,20 @@
+import os
+
+dir_list = ("Core", "Application")
+ext_list = ("h", "cpp", "c")
+
+file_count = 0
+
+print("File list by project:")
+print("===============")
+for directory in dir_list:
+    for (dir,_, files) in os.walk("..\\" + directory):
+                for f in files:
+                        path = os.path.join(dir, f)
+                        if os.path.exists(path) and path.endswith(ext_list):
+                                print(path)
+                                file_count += 1
+print("===============")
+print("Total file count = ", file_count)
+input("")
+
---
+++ Tools/total_line_count.py
@@ -0,0 +1,22 @@
+import os
+
+dir_list = ("Core", "Application")
+ext_list = ("h", "cpp", "c")
+
+total_count = 0
+
+print("Line count by project:")
+print("===============")
+for directory in dir_list:
+    count = 0
+    for (dir,_, files) in os.walk("..\\" + directory):
+                for f in files:
+                        path = os.path.join(dir, f)
+                        if os.path.exists(path) and path.endswith(ext_list):
+                                count += len(open(path).readlines(  ))
+    totalCount +=count
+    print(directory + ": ", count)
+print("===============")
+print("Total count = ", total_count)
+input("")
+
