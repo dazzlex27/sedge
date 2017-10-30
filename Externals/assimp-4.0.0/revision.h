a89514818720c38bf28c8ea356d891e886950e63
dazzlex27 <dazzlex27@mail.ru>
2017-10-30 10:54:11 -0700

    Replaced Assimp sources with binaries.

--- Externals/assimp-4.0.0/revision.h
+++
@@ -1,7 +0,0 @@
-#ifndef ASSIMP_REVISION_H_INC
-#define ASSIMP_REVISION_H_INC
-
-#define GitVersion 0xad761b9
-#define GitBranch "experimental2"
-
-#endif // ASSIMP_REVISION_H_INC
--- Externals/assimp-4.0.0/test/headercheck/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Dev/!Repo/S3DGE/Externals/assimp-4.0.0/test/headercheck
-# Build directory: D:/Dev/!Repo/S3DGE/Externals/assimp-4.0.0/test/headercheck
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
